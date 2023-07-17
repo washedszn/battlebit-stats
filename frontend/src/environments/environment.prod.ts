6d267a6b43eec463bd5607112d9b746bdbaf7f91
h.wright <h.wright@agrovision.com>
2023-07-17 15:15:06 +0200

    fixed environment

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,10 +1,5 @@
-// export const environment = {
-//     production: true,
-//     API_URL: 'https://api.battlebitstats.xyz',
-//     WS_URL: 'wss://api.battlebitstats.xyz'
-// };
 export const environment = {
     production: true,
-    API_URL: 'http://localhost:8000',
-    WS_URL: 'ws://localhost:8001'
+    API_URL: 'https://api.battlebitstats.xyz',
+    WS_URL: 'wss://api.battlebitstats.xyz'
 };
